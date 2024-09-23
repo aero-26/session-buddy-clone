a04aa9942f70f3509af3a32236edb24dffe97e02
aero-26 <shubhankarbanerjee16@gmail.com>
2024-09-23 21:52:20 +0530

    Finished the Project and made it responsive till tablet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const body = document.querySelector("body");
+const body = document.querySelector("html");
 let editBtn = document.querySelectorAll(".edit-btn");
 
 let linkCloseBtn = document.querySelectorAll(".delete");
@@ -8,30 +8,58 @@ const popup = document.querySelectorAll(".container-popup")[0];
 const popupTitle = popup.querySelector("input");
 const popupTextArea = popup.querySelector("textarea");
 const closePopUp = document.querySelector(".popup-close");
-const addBtn = document.querySelector(".add");
+let addBtn = document.querySelector(".add");
 const addPopupBtn = document.querySelector(".add-open");
 const linkContainer = document.querySelector(".container-links");
+let h1 = document.querySelectorAll("h1");
+let ul = document.querySelectorAll("ul");
 let link = document.querySelectorAll(".link");
+let openLinkBtn = document.querySelectorAll(".openLink");
+
+let emptyIndex;
+
+// Reselecting the elements to activate them dynamially whenever body is clicked. Also adding Local Storage Function.
+
+const activateAllBtns = () => {
+  ul = document.querySelectorAll("ul");
+  h1 = document.querySelectorAll("h1");
+  link = document.querySelectorAll(".link");
+  editBtn = document.querySelectorAll(".edit-btn");
+  linkCloseBtn = document.querySelectorAll(".delete");
+  openLinkBtn = document.querySelectorAll(".openLink");
+  addBtn = document.querySelector(".add");
+  activateAddBtn();
+  activateOpenLinkBtn();
+  activateEditBtn();
+  closeBtnUpdate();
+  createLocalStorage();
+};
+
+body.addEventListener("click", () => {
+  activateAllBtns();
+});
+
+// Hiding Popup by default
+popup.classList.add("hidden");
+
+// Preventing the press of spacebar in Textarea
+popupTextArea.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    e.preventDefault();
+  }
+});
 
 // Applying click event to EDIT button and Close button
 const activateEditBtn = () => {
   for (let i = 0; i < link.length; i++) {
     editBtn[i]?.addEventListener("click", () => {
       openDeatils(i);
-    });
-  }
-};
-
-const activateCloseBtn = () => {
-  for (let i = 0; i < link.length; i++) {
-    linkCloseBtn[i]?.addEventListener("click", () => {
-      console.log(link[i]);
+      emptyIndex = i;
     });
   }
 };
 
 activateEditBtn();
-activateCloseBtn();
 
 // Edit button function
 const openDeatils = (i) => {
@@ -51,7 +79,7 @@ const openDeatils = (i) => {
     urlsArray.push(urlsList[i].querySelector("a").innerText);
   }
 
-  // Setting up dynamic size of the textbox
+  // Setting up dynamic size of the textbox as well
   if (urlsArray.lenght <= 8) {
     popupTextArea.style.height = urlsArray.length + 2 + "rem";
   } else {
@@ -81,18 +109,25 @@ const showPopUp = () => {
 };
 
 // Add button functionality
-addBtn.addEventListener("click", () => {
-  showPopUp();
-  disableClick();
-});
+const activateAddBtn = () => {
+  addBtn.addEventListener("click", () => {
+    showPopUp();
+    disableClick();
+  });
+};
 
 // Clicking to Edit/Add btn within popup
 addPopupBtn.addEventListener("click", () => {
+  createOreditNewLinks();
+  closePopUp.click();
+});
+
+const createOreditNewLinks = () => {
   pushLinks();
   templateForLinks(arrForNewElement);
   activateEditBtn();
-  closePopUp.click();
-});
+  activateOpenLinkBtn();
+};
 
 // Function to push array objects from text area
 let arrForNewElement = [];
@@ -136,22 +171,110 @@ const templateForLinks = (arr) => {
             ${finalLi} 
           </ul>
         </div>
-        <button class="add-open">Open All</button>`;
+        <button class="add-open openLink">Open All</button>`;
 
   // Adding condition to add the links
-  if (addPopupBtn.innerText === "Add") {
+  if (
+    addPopupBtn.innerText === "Add" &&
+    tempTemplate.querySelector("h1").textContent !== "" &&
+    tempTemplate.querySelector("a").textContent
+  ) {
+    console.log(tempTemplate.querySelector("a").textContent);
     linkContainer.insertAdjacentElement("afterbegin", tempTemplate);
+  }
+
+  if (addPopupBtn.innerText === "Done") {
+    console.log(popupTitle.value);
+    console.log(popupTextArea.value.split("\n"));
+    h1[emptyIndex].textContent = popupTitle.value;
+    let liArr = [];
+    popupTextArea.value.split("\n").forEach((value) => {
+      let tempLi = `<li>
+        <a
+          href="${value}"
+          target="_blank"
+          title="${value}"
+        >
+         ${value}
+        </a>
+      </li>`;
+      liArr.push(tempLi);
+    });
+    ul[emptyIndex].innerHTML = liArr.join("\n");
+    console.log(emptyIndex);
   }
 
   console.log(tempTemplate);
 
-  // Reselecting the elements to activate them dynamially
-  link = document.querySelectorAll(".link");
-  editBtn = document.querySelectorAll(".edit-btn");
-  linkCloseBtn = document.querySelectorAll(".delete");
-  activateEditBtn();
-  activateCloseBtn();
-
   // Emptying the array
   arrForNewElement = [];
 };
+
+// Adding functionality to Open Link button
+
+const activateOpenLinkBtn = function () {
+  for (let i = 0; i < openLinkBtn.length; i++) {
+    openLinkBtn[i].addEventListener("click", openLink);
+  }
+};
+
+function openLink() {
+  let recordIndex;
+  for (let i = 0; i < openLinkBtn.length; i++) {
+    if (this === openLinkBtn[i]) {
+      recordIndex = i;
+    }
+  }
+
+  let links = ul[recordIndex].querySelectorAll("li");
+  for (let i = 0; i < links.length; i++) {
+    links[i].querySelector("a").click();
+  }
+}
+
+// Add close set functionality or remove component
+const closeBtnUpdate = () => {
+  for (const btn of linkCloseBtn) {
+    btn.addEventListener("click", removeComponent);
+  }
+};
+
+const removeComponent = function () {
+  for (let i = 0; i <= linkCloseBtn.length; i++) {
+    if (this === linkCloseBtn[i]) {
+      link[i].remove();
+    }
+  }
+};
+
+// Implementation of LocalStorage
+
+const createLocalStorage = () => {
+  let tempLinkDivs = [];
+  for (const linkDiv of link) {
+    if (linkDiv !== addBtn) {
+      tempLinkDivs.push(`
+      <div class="link">
+      ${linkDiv.innerHTML}
+      </div>`);
+    } else if (linkDiv === addBtn) {
+      tempLinkDivs.push(`
+      <div class="link add">
+      ${linkDiv.innerHTML}
+      </div>`);
+    }
+  }
+
+  const tempLinkToString = tempLinkDivs.join("\n");
+  localStorage.setItem("Save", tempLinkToString);
+  tempLinkDivs = [];
+};
+
+const checkLocalStorage = () => {
+  if (localStorage.getItem("Save") !== "" && localStorage.getItem("Save")) {
+    linkContainer.innerHTML = localStorage.getItem("Save");
+    activateAllBtns();
+  }
+};
+
+checkLocalStorage();
